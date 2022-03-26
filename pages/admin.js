33a5c753d36d4769b84fddea8af48054ccc06fcf
dave-alt <76473751+dave-alt@users.noreply.github.com>
2022-03-26 11:29:46 +0530

    uploaded new files

---
+++ pages/admin.js
@@ -0,0 +1,33 @@
+import Moment from "react-moment";
+import { useEffect, useState } from "react";
+import axios from "axios"
+export default function Admin() {
+  const[details,setDetails]=useState([]);
+
+  useEffect(() => {
+    axios.get('https://cangeo.herokuapp.com/api/v1/lead/get')
+        .then(response =>{
+          setDetails(response.data)
+          
+         
+        });
+  }, []);
+
+  return (
+    <>
+        {
+          details.map((detail) =>{
+            return(<>
+              <Moment format="MMMM Do YYYY">{detail.createdAt}</Moment>
+                <h1>{detail.name}</h1>
+                <h1>{detail.email}</h1>
+                <h1>{detail.message}</h1>
+              <br/>
+            </>)
+          })
+        }
+  
+     
+      </>
+  );
+}
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Home from '../components/home'
+import Form from '../components/form'
+
+export default function Index() {
+  return (
+    <>
+  <Home/> 
+  <Form/>
+  </>
+    )
+}
