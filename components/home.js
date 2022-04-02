f6e12ccfa327bccc5affb9a13049a2eb1cd5a57a
dave-alt <76473751+dave-alt@users.noreply.github.com>
2022-04-03 00:49:57 +0530

    Add files via upload

--- components/home.js
+++ components/home.js
@@ -1,106 +1,114 @@
-import React from 'react'
+import React from "react";
+import { useEffect } from "react";
+import Technologies from "../components/technologies";
+import Services from "../components/services"
 
-export default function home() {
+export default function Home() {
+  useEffect(() => {}, []);
+
   return (
-    <>  
-    <div className="navContainer">
-        <div className="logoContainer" >
-           <a className="logoLink" href="#heroSection">
-               <img style={{height:'30px'}} src="cangeo.svg" alt="cangeo" />
-           </a>
-        </div>
-        <nav className="navLinksContainer">
-            <a href="#about">About       </a>
-            <a href="#services">Services </a>
-            <a href="#contact">Contact   </a>
-        </nav>
-    </div>
-   
-    
-    <div id="heroSection" className="hero">
+    <>
+      <div id="heroSection" className="hero">
+        <video
+          id="#video"
+          muted
+          autoPlay={"autoplay"}
+          loop
+          src="./m1.mp4"
+        />
+        {/* <div className="backgroundfiller">
+        
+        </div> */}
         <div className="getQuote">
-            <h1>Its hard to find things that won't sell online!</h1>
-            <a href="#contact"><button className="btn"> Get Quote</button></a>
-            
+          <h1>Elevate your Business with CANGEO's Digital Marketing Solutions</h1>
+          <a href="#contact">
+            <button className="btn"> Get Quote</button>
+          </a>
         </div>
-        <img className="heroImage" src="hero_image.svg" alt="" />
-    </div>
-   
-    <div id="about" className="about">
-    <h1 className="about_heading">About Us</h1>
-        <div className="aboutContent">
-            <img src="./about.svg" alt="about image" className="about_image"/>
-            <p><b>Cangeo</b> is a Tech firm located in Canada.
-            <br/><br/>We specialize in providing digital marketing solutions to your business needs.
-                <br/><br/>Its crucial to add status-quo marketing technologies in your toolbelt. Cangeo takes care of it. 
-        </p>
-        </div>
-    </div>
-   
+      </div>
 
-    <div id="services" className="services">
+        <div className="theServices">
+        <h1>Our services</h1>
+          <Services/>
+        </div>
+
+
+      {/* <div id="services" className="services">
         <h1>Our services</h1>
         <div className="cardsContainer">
-            <div className="card-container">
-                <div className="assetContainer">
-                <img src="./seo.svg" alt="" />
-                </div>
-                <div className="tsf">Search Engine Optimization</div>
+          <div className="card-container">
+            <div className="assetContainer">
+              <img src="./digital.svg" alt="" />
             </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                  <img src="./email.svg" alt="" />
-                </div>
-                <div className="tsf">E-Mail Campaigning</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                  <img src="./ux.svg" alt="" />
-                </div>
-                <div className="tsf">UX/UI/Graphic Design</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                  <img src="./video.svg" alt="" />
-                </div>
-                <div className="tsf">Videography</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                    <img src="./web.svg" alt="" />
-                </div>
-                <div className="tsf">Web/App Development</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                    <img src="./data.svg" alt="" />
-                </div>
-                <div className="tsf">Data Entry</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                    <img src="./medical.svg" alt="" />
-                </div>
-                <div className="tsf">Medical Coding</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                    <img src="./bill.svg" alt="" />
-                </div>
-                <div className="tsf">Bill Generation</div>
-            </div>
-            <div className="card-container">
-                <div className="assetContainer">
-                    <img src="./content.svg" alt="" />
-                </div>
-                <div className="tsf">Content Creation</div>
-            </div>
-        </div>
-    </div>
-  
+            <div className="tsf">Digital Marketing</div>
+          </div>
 
-    
-    
+          <div className="card-container">
+            <div className="assetContainer">
+              <img src="./ux.svg" alt="" />
+            </div>
+            <div className="tsf">UX/UI/Graphic Design</div>
+          </div>
+          <div className="card-container">
+            <div className="assetContainer">
+              <img src="./video.svg" alt="" />
+            </div>
+            <div className="tsf">Videography</div>
+          </div>
+          <div className="card-container">
+            <div className="assetContainer">
+              <img src="./web.svg" alt="" />
+            </div>
+            <div className="tsf">Web/App Development</div>
+          </div>
+          <div className="card-container">
+            <div className="assetContainer">
+              <img src="./data.svg" alt="" />
+            </div>
+            <div className="tsf">Data Entry/Bill generation</div>
+          </div>
+          <div className="card-container">
+            <div className="assetContainer">
+              <img src="./medical.svg" alt="" />
+            </div>
+            <div className="tsf">Medical Coding</div>
+          </div>
+        </div>
+      </div> */}
+      <div  className="technologies">
+        <h1 className="about_heading">Technologies We Use</h1>
+        <Technologies/>
+      </div>
+     
+      <div id="about" className="about">
+        <div className="aboutContent">
+          <h1 className="about_heading">About Us</h1>
+          <p>
+            <b>CANGEO</b> is a Tech firm located in Canada.
+            <br />
+            <br />
+            We specialize in providing digital marketing solutions to your
+            business needs.
+            <br />
+            <br />
+            Its crucial to add status-quo marketing technologies in your
+            toolbelt. CANGEO takes care of it.
+            <br />
+            <br />
+            We are committed to build best products by following an{" "}
+            <b>User Centered Design</b> approach. We start right from
+            wireframing, and then finalise the product design by various
+            iterations which involves improvements. A modern product should be{" "}
+            <b>Accessible, Reliable , Usable and Enjoyable</b>.
+            <br />
+            <br />
+            We are equipped with server-side rendering technologies so that we
+            can assure that all your products are <b> Search-engine ready</b>.
+            We provide valuable customer support and would love to embark in a
+            long-term relationships.
+          </p>
+        </div>
+      </div>
     </>
-  )
+  );
 }
---
+++ pages/pricing.js
@@ -0,0 +1,117 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../components/navbar";
+import Skyline from "../components/skyline";
+import Footer from "../components/footer";
+import { PricingTable, PricingSlot, PricingDetail } from "react-pricing-table";
+import { useRouter } from "next/router";
+export default function Pricing() {
+  const router = useRouter();
+  let basicComponents = [];
+  let intermediateComponents = [];
+  let advancedComponents = [];
+  let digitalmarketingComponents = [];
+
+  const basic = [
+    "Free Domain",
+    "6 Pages Hosting",
+    "Business Email Id",
+    "SEO Ready Website",
+    "Mobile Friendly Website",
+    "Unlimited Images and Videos",
+    "Unlimited Bandwidth/Space",
+    "24/7 Support",
+  ];
+  const intermediate = [
+    "All from basic",
+    "12 Pages",
+    "Professional Logo",
+    "UI/UX Design",
+    "Fast/Server Side Rendering",
+    "Additional Database Services",
+  ];
+  const digitalMarketing = [
+    "Banner",
+    "Logo/Mascot",
+    "Graphic Design/Animation",
+   
+    "SEO/SMO",
+    "Social Media Marketing",
+    "Email Marketing",
+    "Video Advertising",
+    "Content Marketing",
+    "Google Analytics/Adwords",
+    "Facebook ads",
+  ];
+  const advanced = [
+    "Digital Marketing/Web Development",
+    "25 Pages",
+    "All from INTERMEDIATE",
+    "All from DIGITAL MARKETING",
+  ];
+
+  basic.forEach((detail) => {
+    basicComponents.push(<PricingDetail>{detail}</PricingDetail>);
+  });
+  intermediate.forEach((detail) => {
+    intermediateComponents.push(<PricingDetail>{detail}</PricingDetail>);
+  });
+  advanced.forEach((detail) => {
+    advancedComponents.push(<PricingDetail>{detail}</PricingDetail>);
+  });
+  digitalMarketing.forEach((detail) => {
+    digitalmarketingComponents.push(<PricingDetail>{detail}</PricingDetail>);
+  });
+
+  return (
+    <>
+      <Navbar />
+      <div className="pricing">
+        <PricingTable highlightColor="#198995">
+          <PricingSlot
+            buttonText="SIGN UP"
+            onClick={() => router.push("/#contact")}
+            title="BASIC"
+            priceText="$300 + $100 Annual"
+          >
+            {basicComponents}
+          </PricingSlot>
+          <PricingSlot
+            buttonText="SIGN UP"
+            onClick={() => router.push("/#contact")}
+            title="INTERMEDIATE"
+            priceText="$500 + $150 Annual"
+          >
+          {intermediateComponents}
+          </PricingSlot>
+          <PricingSlot
+            buttonText="SIGN UP"
+            onClick={() => router.push("/#contact")}
+            title="DIGITAL MARKETING"
+            priceText="$600 + $100 Annual"
+          >
+           {digitalmarketingComponents}
+          </PricingSlot>
+          <PricingSlot
+            buttonText="SIGN UP"
+            onClick={() => router.push("/#contact")}
+            title="E-COMMERCE"
+            priceText="$1500 + $100 Annual"
+          >
+           
+          </PricingSlot>
+          
+          <PricingSlot
+            buttonText="SIGN UP"
+            onClick={() => router.push("/#contact")}
+            title="ADVANCED"
+            priceText="$1500 + $150 Annual"
+          >
+           {advancedComponents}
+          </PricingSlot>
+        </PricingTable>
+      </div>
+      
+      <Footer/>
+    </>
+  );
+}
